e5420275fb1dc979bdee4d475365ec5129d758f9
mowbraym <33804198+mowbraym@users.noreply.github.com>
2018-01-30 17:55:20 +1100

    Initial Load
    1 x table and 1 x view only plus ancilliary files

---
+++ LumapacS.User.sql
@@ -0,0 +1,5 @@
+USE [dswMLM]
+GO
+/****** Object:  User [LumapacS]    Script Date: 30/01/2018 5:54:18 PM ******/
+CREATE USER [LumapacS] FOR LOGIN [LumapacS] WITH DEFAULT_SCHEMA=[dbo]
+GO
---
+++ dbo.Table_1.Table.sql
@@ -0,0 +1,14 @@
+USE [dswMLM]
+GO
+/****** Object:  Table [dbo].[Table_1]    Script Date: 30/01/2018 5:54:19 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Table_1](
+	[c1] [nchar](10) NULL,
+	[c2] [nchar](10) NULL,
+	[c3] [nchar](10) NULL
+) ON [PRIMARY]
+
+GO
